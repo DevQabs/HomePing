95df77751df565f9a6f82f2f82e6aeb705ad52c8
devqabs <devqabs@gmail.com>
2021-10-30 10:36:18 +0900

    css -> styled-component
    pesudo component → react component로 전환

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
 import './App.css';
+import styled from 'styled-components';
+
+const Tile = styled.div`
+  width: 200px;
+  height: 200px;
+  float: left;
+  margin: 0 10px 10px 0;
+  cursor: pointer;
+
+  &:before {
+    position: absolute;
+    content: '';
+    width: 200px;
+    height: 200px;
+    background: white;
+    -webkit-transition: all 0.3s;
+    transition: all 0.3s;
+    pointer-events: none;
+  }
+
+  &:hover:before {
+    background: #ffc600;
+    -webkit-transform: translate3d(0, 0, 50px);
+    transform: translate3d(0, 0, 5px);
+    box-shadow: 30px 30px 10px rgba(0, 0, 0, 0.5);
+  }
+`;
 
 function App() {
   return (
     <div className='App'>
       <p>Hover over</p>
       <div id='Tiles'>
-        {[...Array(900)].map(() => {
-          return <div class='tile' />;
+        {[...Array(4)].map((e, i) => {
+          return (
+            // <div className='tile' key={`tile_${i}`}>
+            //   aaaaa
+            // </div>
+            <Tile />
+          );
         })}
       </div>
+
+      <div
+        draggable
+        style={{ width: '20px', height: '20px', backgroundColor: 'red' }}
+      />
     </div>
   );
 }
